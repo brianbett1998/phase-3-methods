2c516bcd3f1a4ff733785f0a5aa8dd31feb6df2e
BRIAN BETT <+>
2023-05-17 11:25:11 +0300

    Completed'

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name=Naureen)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+
+def add (num1, num2)
+    return 2 + 5
+end
+
+def halve(number)
+    return nil unless number.is_a?(Numeric)
+    
+    number / 2
+end
+  
